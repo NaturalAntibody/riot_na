0f114b176b926aab7133101b0d0340abc5f42866
Bartosz Janusz <bartosz.janusz@naturalantibody.com>
2024-10-09 18:00:38 +0200

    feat: added utils module with some commonly used functions

--- riot_na/__init__.py
+++ riot_na/__init__.py
@@ -8,9 +8,16 @@ from riot_na.api.riot_numbering import (
     RiotNumberingNT,
     create_riot_aa,
     create_riot_nt,
-    Organism,
-    Scheme,
 )
 from riot_na.api.api_mp import run_on_file_mp
 from riot_na.data.scheme_regions import get_regions_definitions
-from riot_na.data.model import AirrRearrangementEntryNT, AirrRearrangementEntryAA
+from riot_na.data.scheme_definitions import Regions, ChainRegions
+from riot_na.data.model import (
+    AirrRearrangementEntryNT,
+    AirrRearrangementEntryAA,
+    ShortRegion,
+    ChainType,
+    Scheme,
+    Organism,
+)
+from riot_na.api import utils
