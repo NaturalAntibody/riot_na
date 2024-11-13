07df2bef41744fcbc4e5c4b8496e029cc72f0f96
semantic-release <semantic-release>
2024-11-13 11:54:37 +0000

    bump: 1.2.3 [skip ci]
    Automatically generated by python-semantic-release

--- riot_na/__init__.py
+++ riot_na/__init__.py
@@ -1,7 +1,7 @@
 # pylint: skip-file
 # flake8: noqa
 # isort: skip_file
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 from riot_na.riot_na import Prefiltering
 from riot_na.api.riot_numbering import (
     RiotNumberingAA,
